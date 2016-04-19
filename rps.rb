d96b9752a78c2f02c85ec5afb99ccda62915f406
rddennis <ronesha_dennis@yahoo.com>
2016-04-19 09:56:07 -0500

    updated rps to make it work

--- rps.rb
+++ rps.rb
@@ -11,7 +11,7 @@ class RockPaperScissors
   def play
     human_move = human_selection
     computer_move = computer_selection
-    get_winner (human_move, computer_move)
+    get_winner(human_move, computer_move)
   end
 
   def human_selection
@@ -27,13 +27,15 @@ class RockPaperScissors
     if input1 == input2
       "It's a tie! Play again!"
     else
-      if WINS.include([input1, input2])
-        "Computer shot #{input2.capitalize}. You shot #{input1.capitalize}! You win!"
+      if WINS.include?([input1, input2])
+        puts "Computer shot #{input2.capitalize}. You shot #{input1.capitalize}! You win!"
       else
-        "Computer shot #{input2.capitalize}. You shot #{input1.capitalize}. You lost!"
+        puts "Computer shot #{input2.capitalize}. You shot #{input1.capitalize}. You lost!"
       end
     end
-
   end
 
 end
+
+game=RockPaperScissors.new
+game.play
